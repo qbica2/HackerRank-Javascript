9893cf73b3ac6aa05fdaebd77a28676d6a03e2c7
kubilay <kubidik27@gmail.com>
2021-10-21 20:20:45 +0300

    day3-array

---
+++ 10-Days-/day3-arrays/arrays.js
@@ -0,0 +1,16 @@
+
+let nums = [2,4,6,6,5]      // içeriği değiştirebilirsiniz
+
+console.log(Math.max(...nums)       //math max ile dizideki en büyük sayıyı bulduk
+)
+
+function getSecondLargest(nums) {
+     let filterednums = nums.filter(function(notmax){   //filterla en büyük sayının olduğu ögeleri(2tane 6) 
+         return notmax < Math.max(...nums);             // çıkarıp yeni dizi elde ettik 
+     })
+     let secondLargets= Math.max(...filterednums)       // artık yeni dizimizin en büyük sayısına 
+     return secondLargets ;                             // ulaşabiliriz
+     
+}
+
+
